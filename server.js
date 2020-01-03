d506772f453c1685c0e091bde15751760db98f74
Thav0 <gustavodnz035@gmail.com>
2020-01-03 19:08:54 -0300

    Last commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express       = require('express')
+const mongoose      = require('mongoose')
+const requireDir    = require('require-dir')
+const cors          = require('cors')
+
+// start app
+const app           = express()
+app.use(express.json())
+app.use(cors())
+
+// database
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    {useNewUrlParser: true}
+)
+
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
+
+// route
+app.use('/api', require('./src/routes'))
+
+app.listen(3001);
